db0bb2b6678aede057dcf6723d0afa10c4f6ef67
nathannuness1 <00001089566268sp@al.educacao.sp.gov.br>
2024-11-06 11:25:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Língua Portuguesa',
+    'Qual é a maior palavra da língua portuguesa?',
+    'Pneumoultramicroscopicossilicovulcanoconiótico'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior ladrão da história do Brasil?',
+    'Lula'
+)
+
+criaCartao(
+    'Fatos interessantes',
+    'Quem é a pessoa com o maior número de seguidores do mundo?',
+    'Cristiano Ronaldo'
+)
+
+criaCartao(
+    'Religião',
+    'Quem traiu Jesus?',
+    'Judas'
+)
